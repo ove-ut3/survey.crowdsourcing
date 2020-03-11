87d9a2086298ad58fa72a014eba5813420b2d49f
Stéphane Le Hénanff <github@le-henanff.fr>
2020-03-11 09:41:06 +0100

    set height for selectize input to 36px

--- R/app_ui.R
+++ R/app_ui.R
@@ -23,16 +23,16 @@ app_ui <- function() {
 #' @import shiny
 golem_add_external_resources <- function(){
   
-  # addResourcePath(
-  #   'www', system.file('app/www', package = 'survey.crowdsourcing')
-  # )
+  addResourcePath(
+    'www', system.file('app/www', package = 'survey.crowdsourcing')
+  )
  
   tags$head(
     golem::activate_js(),
-    golem::favicon()
+    golem::favicon(),
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
-    #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+    tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
   )
 }
